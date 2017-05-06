89c052df56691877e1094dfcf79bef8a66ffa863
QiqiDuan <2332917051@qq.com>
2017-05-06 16:07:59 +0800

    v00.00.004
    format

--- CUDA/docs/learn_cuda.cu
+++ CUDA/docs/learn_cuda.cu
@@ -130,65 +130,65 @@ void call_devAddVect( void ) {
 
 void call_devAddVectArbitraryLength( void ) {
 	/* add two vectors with arbitrary length */
-		printf( "\n*********************\n* add two vectors with arbitrary length *\n*********************\n" );
-		int ind_dev = 1;
-		HANDLE_CUDA_ERROR( cudaSetDevice( ind_dev) );
+	printf( "\n*********************\n* add two vectors with arbitrary length *\n*********************\n" );
+	int ind_dev = 1;
+	HANDLE_CUDA_ERROR( cudaSetDevice( ind_dev) );
 
-		unsigned vect_length = 100000000;
-		size_t vect_length_bytes = vect_length * sizeof( double );
-		double *vect_a = NULL, *vect_b = NULL, *vect_c = NULL;
-		vect_a = ( double * ) malloc( vect_length_bytes );
-		vect_b = ( double * ) malloc( vect_length_bytes );
-		vect_c = ( double * ) malloc( vect_length_bytes );
-		if ( vect_a == NULL || vect_b == NULL || vect_c == NULL ) {
-			fprintf( stderr, "\nHOST ERROR :: cannot allocate enough memory.\n" );
-		}
-		for ( unsigned i = 0; i < vect_length; i++ ) {
-			vect_a[ i ] = i;
-			vect_b[ i ] = 2 * i;
-		}
+	unsigned vect_length = 100000000;
+	size_t vect_length_bytes = vect_length * sizeof( double );
+	double *vect_a = NULL, *vect_b = NULL, *vect_c = NULL;
+	vect_a = ( double * ) malloc( vect_length_bytes );
+	vect_b = ( double * ) malloc( vect_length_bytes );
+	vect_c = ( double * ) malloc( vect_length_bytes );
+	if ( vect_a == NULL || vect_b == NULL || vect_c == NULL ) {
+		fprintf( stderr, "\nHOST ERROR :: cannot allocate enough memory.\n" );
+	}
+	for ( unsigned i = 0; i < vect_length; i++ ) {
+		vect_a[ i ] = i;
+		vect_b[ i ] = 2 * i;
+	}
 
-		double *dev_vect_a, *dev_vect_b, *dev_vect_c;
-		HANDLE_CUDA_ERROR( cudaMalloc( ( double ** ) &dev_vect_a, vect_length_bytes ) );
-		HANDLE_CUDA_ERROR( cudaMalloc( ( double ** ) &dev_vect_b, vect_length_bytes ) );
-		HANDLE_CUDA_ERROR( cudaMalloc( ( double ** ) &dev_vect_c, vect_length_bytes ) );
+	double *dev_vect_a, *dev_vect_b, *dev_vect_c;
+	HANDLE_CUDA_ERROR( cudaMalloc( ( double ** ) &dev_vect_a, vect_length_bytes ) );
+	HANDLE_CUDA_ERROR( cudaMalloc( ( double ** ) &dev_vect_b, vect_length_bytes ) );
+	HANDLE_CUDA_ERROR( cudaMalloc( ( double ** ) &dev_vect_c, vect_length_bytes ) );
 
-		HANDLE_CUDA_ERROR( cudaMemcpy( dev_vect_a, vect_a, vect_length_bytes, cudaMemcpyHostToDevice ) );
-		HANDLE_CUDA_ERROR( cudaMemcpy( dev_vect_b, vect_b, vect_length_bytes, cudaMemcpyHostToDevice ) );
+	HANDLE_CUDA_ERROR( cudaMemcpy( dev_vect_a, vect_a, vect_length_bytes, cudaMemcpyHostToDevice ) );
+	HANDLE_CUDA_ERROR( cudaMemcpy( dev_vect_b, vect_b, vect_length_bytes, cudaMemcpyHostToDevice ) );
 
-		unsigned num_thread_per_block = 1024;
-		unsigned num_block_per_grid = 1024;
-		time_t gpu_run_time_start, gpu_run_time_end;
-		time( &gpu_run_time_start );
-		devAddVectArbitraryLength <<< num_block_per_grid, num_thread_per_block >>> (
-				dev_vect_a, dev_vect_b, dev_vect_c, vect_length );
-		time( &gpu_run_time_end );
-		double gpu_run_time = difftime( gpu_run_time_end, gpu_run_time_start );
-		HANDLE_CUDA_ERROR( cudaPeekAtLastError() );
-		HANDLE_CUDA_ERROR( cudaMemcpy( vect_c, dev_vect_c, vect_length_bytes, cudaMemcpyDeviceToHost ) );
+	unsigned num_thread_per_block = 1024;
+	unsigned num_block_per_grid = 1024;
+	time_t gpu_run_time_start, gpu_run_time_end;
+	time( &gpu_run_time_start );
+	devAddVectArbitraryLength <<< num_block_per_grid, num_thread_per_block >>> (
+			dev_vect_a, dev_vect_b, dev_vect_c, vect_length );
+	time( &gpu_run_time_end );
+	double gpu_run_time = difftime( gpu_run_time_end, gpu_run_time_start );
+	HANDLE_CUDA_ERROR( cudaPeekAtLastError() );
+	HANDLE_CUDA_ERROR( cudaMemcpy( vect_c, dev_vect_c, vect_length_bytes, cudaMemcpyDeviceToHost ) );
 
-		printf( "Totoal GPU run time: %lf.\n", gpu_run_time );
-		for ( unsigned i = 0; i < vect_length; i++ ) {
-			if ( is_equal( vect_c[ i ], vect_a[ i ] + vect_b[ i ] ) == 0 ) {
-				printf( "\nError :: final results are not right.\n" );
-				printf( ">> i = %u :: vect_c[ i ] = %lf vs. vect_a[ i ] + vect_b[ i ] = %lf.\n",
-						i, vect_c[ i ], vect_a[ i ] + vect_b[ i ] );
-				exit( EXIT_FAILURE );
-			} else {
-				if ( i == 0 || i == ( vect_length / 2 ) || i == ( vect_length - 1 ) ) {
-					printf( ">> i = %u :: vect_c (%.2lf) == vect_a (%.2lf) + vect_b (%.2lf).\n",
-							i, vect_c[ i ], vect_a[ i ], vect_b[ i ] );
-				}
+	printf( "Totoal GPU run time: %lf.\n", gpu_run_time );
+	for ( unsigned i = 0; i < vect_length; i++ ) {
+		if ( is_equal( vect_c[ i ], vect_a[ i ] + vect_b[ i ] ) == 0 ) {
+			printf( "\nError :: final results are not right.\n" );
+			printf( ">> i = %u :: vect_c[ i ] = %lf vs. vect_a[ i ] + vect_b[ i ] = %lf.\n",
+					i, vect_c[ i ], vect_a[ i ] + vect_b[ i ] );
+			exit( EXIT_FAILURE );
+		} else {
+			if ( i == 0 || i == ( vect_length / 2 ) || i == ( vect_length - 1 ) ) {
+				printf( ">> i = %u :: vect_c (%.2lf) == vect_a (%.2lf) + vect_b (%.2lf).\n",
+						i, vect_c[ i ], vect_a[ i ], vect_b[ i ] );
 			}
 		}
+	}
 
-		HANDLE_CUDA_ERROR( cudaFree( dev_vect_a ) );
-		HANDLE_CUDA_ERROR( cudaFree( dev_vect_b ) );
-		HANDLE_CUDA_ERROR( cudaFree( dev_vect_c ) );
-		free( vect_a );
-		free( vect_b );
-		free( vect_c );
-		cudaDeviceReset();
+	HANDLE_CUDA_ERROR( cudaFree( dev_vect_a ) );
+	HANDLE_CUDA_ERROR( cudaFree( dev_vect_b ) );
+	HANDLE_CUDA_ERROR( cudaFree( dev_vect_c ) );
+	free( vect_a );
+	free( vect_b );
+	free( vect_c );
+	cudaDeviceReset();
 }
 
 
